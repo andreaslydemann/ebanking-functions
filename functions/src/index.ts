769d3f839725803fff219ed1fab00c22e86514f2
Andreas Lüdemann <andreaslydemann@gmail.com>
2018-04-03 12:46:08 +0200

    Add service account

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,11 @@
-import * as functions from 'firebase-functions';
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+const serviceAccount = require('./config/service_account');
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`
+});
 
 export const helloWorld = functions.https.onRequest((request, response) => {
     response.send("Hello from Firebase!");
