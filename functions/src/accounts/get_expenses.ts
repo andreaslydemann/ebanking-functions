4d62b98dc54b1d3dc862183b0259785e7a4d4dd3
VictorBusk <victorbusk@hotmail.com>
2018-04-12 19:06:07 +0200

    Now handling JSON

--- functions/src/accounts/get_expenses.ts
+++ functions/src/accounts/get_expenses.ts
@@ -5,31 +5,21 @@ const dateHelper = require('../helpers/date_helper');
 
 module.exports = function (req, res) {
     cors(req, res, async () => {
-        const token = req.get('Authorization').split('Bearer ')[1];
-        try {
-            await admin.auth().verifyIdToken(token);
-        } catch (err) {
-            res.status(401).send({error: "Brugeren kunne ikke verificeres."});
-        }
 
         if (!req.query.accountIDs || !req.query.from || !req.query.to)
             return res.status(400).send({error: 'Fejl i anmodningen.'});
 
         const db = admin.firestore();
-        const accountIDs = req.query.accountIDs;
-        console.log(accountIDs);
+        const acoountIDs = JSON.parse(req.query.accountIDs);
         const from = String(
             dateHelper.toDateString(new Date(req.query.from)));
-        console.log("from " + from);
         const to = String(
             dateHelper.toDateString(new Date(req.query.to)));
-        console.log("to " + to);
-
         const getExpensesPromises = [];
         const sortedExpenses = [];
 
         const expenses = [];
-        accountIDs.forEach((accountID) => {
+        acoountIDs.forEach(accountID => {
             const getExpensesPromise = db.collection("expenses")
                 .where("accountID", "==", accountID)
                 .get()
@@ -37,18 +27,17 @@ module.exports = function (req, res) {
                     accountExpenses.forEach((expense) => {
                         expenses.push({
                             amount: expense.data().amount,
-                            categoryTypeID: expense.data().categoryTypeID
+                            categoryTypeID: expense.data().categoryTypeID,
+                            createdAt: expense.data().createdAt
                         })
                     })
                 });
             getExpensesPromises.push(getExpensesPromise);
         });
 
-        console.log("Hej 1");
-
         await Promise.all(getExpensesPromises);
         expenses.forEach((expense) => {
-            if (expense.createdAT >= from && expense.createdAT <= to) {
+            if (expense.createdAt >= from && expense.createdAt <= to) {
                 sortedExpenses.push({
                     amount: expense.amount,
                     categoryTypeID: expense.categoryTypeID
